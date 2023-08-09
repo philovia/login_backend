ab245bac1bbf14e7164481ac3f5c18bbfdf8b1e9
FDSAP-Intern6-Macazar <fdsap-intern6-macazar@FDSAP-Intern5.local>
2023-08-09 16:59:13 +0800

    Login UI with Backend

---
+++ lib/data/network/BaseApiServices.dart
@@ -0,0 +1,8 @@
+
+
+abstract class BasApiServices {
+
+  Future<dynamic> getGetApiResponse(String url);
+
+  Future<dynamic> getPostApiResponse(String url , dynamic data);
+}
---
+++ lib/data/response/api_response.dart
@@ -0,0 +1,28 @@
+
+import 'package:loginwbackend/data/response/status.dart';
+
+class ApiResponse<T> {
+
+  Status? status ;
+  T? data ;
+  String? message ;
+
+  ApiResponse(this.status , this.data , this.message);
+
+  ApiResponse.loading() : status = Status.LOADING ;
+
+  ApiResponse.completed() : status = Status.COMPLETED ;
+
+  ApiResponse.error() : status = Status.ERROR ;
+
+  ApiResponse.atto() : status = Status.attp ;
+
+  @override
+  String toString(){
+    return "Status : $status \n Message : $message \n Data: $data" ;
+  }
+
+}
+
+
+
